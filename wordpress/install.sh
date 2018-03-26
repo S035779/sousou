446b32071552e3d67e91959235d1ec53561b1e6c
Mamoru Hashimoto <you@example.com>
2018-03-26 21:14:34 +0900

    modified wordpress/install.sh

--- wordpress/install.sh
+++ wordpress/install.sh
@@ -3,20 +3,13 @@ ASSETS=/home/fwpresearch/public_html/assets
 PLUGIN=/home/fwpresearch/public_html/dev/wp-content/plugins/paypal-shortcode
 TODAY=$(date "+%Y%m%d")
 
-mv $ASSETS/js/app.bundle.js        $ASSETS/js/app.bundle_${TODAY}.js
-mv $ASSETS/js/widget.bundle.js     $ASSETS/js/widget.bundle_${TODAY}.js
-mv $ASSETS/js/app.bundle.js.map    $ASSETS/js/app.bundle_${TODAY}.js.map
-mv $ASSETS/js/widget.bundle.js.map $ASSETS/js/widget.bundle_${TODAY}.js.map
-cp dist/*.js $ASSETS/js/
-cp dist/*.js.map $ASSETS/js/
-
-mv $ASSETS/css/style.css     $ASSETS/css/style_${TODAY}.css
-mv $ASSETS/css/style.css.map $ASSETS/css/style_${TODAY}.css.map
-cp dist/*.css $ASSETS/css/
-cp dist/*.css.map $ASSETS/css/
-
+mv $ASSETS/js/app.bundle.js      $ASSETS/js/app.bundle_${TODAY}.js
+mv $ASSETS/js/widget.bundle.js   $ASSETS/js/widget.bundle_${TODAY}.js
+mv $ASSETS/css/style.css         $ASSETS/css/style_${TODAY}.css
 mv $ASSETS/image/myanmaphoto.png $ASSETS/image/myanmaphoto_${TODAY}.png
+cp dist/*.js  $ASSETS/js/
+cp dist/*.css $ASSETS/css/
 cp dist/*.png $ASSETS/image/
 
-mv $PLUGIN/paypal-shortcode.php $PLUGIN/paypal-shortcode_${TODAY}.php
+mv $PLUGIN/paypal-shortcode.php   $PLUGIN/paypal-shortcode_${TODAY}.php
 cp wordpress/paypal-shortcode.php $PLUGIN/
